f4154fc26c8600f004e86fcef274295c53ac6f56
Bhavani Goruganthu <bhavani.goruganthu@mail.sfsu.edu>
2021-11-14 16:50:24 -0800

    added login route

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/home" element={<MenuBar />} />
       </Routes>
