7111bcf3f1fec1568011d3bfd9525f52a491054e
Bhavani Goruganthu <bhavani.goruganthu@mail.sfsu.edu>
2021-12-07 01:51:33 -0800

    modified the query to pull for specific email

--- backendServer/endpoints/user.js
+++ backendServer/endpoints/user.js
@@ -7,7 +7,8 @@ router.use(express.json());
 // Customer log in
 router.get('/login', (req, res) => {
   console.log('Called user login endpoint');
-  let query = `SELECT * FROM user`;
+  let query =
+    `SELECT * FROM user WHERE login_username = "` + req.query.userEmail + `"`;
   database.query(query, (err, result) => {
     res.send(result);
   });
